d11e2da922283eccadb0b8667f793256e044c3ef
Max Schoening <max@max.wtf>
2018-02-14 16:13:51 -0800

    Add Stream

---
+++ components/main.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div>
+    <h2>This is the main area</h2>
+  </div>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import Main from '../components/main'
+import Stream from '../components/stream'
+
 export default () => (
   <div>
     <h1>PBJ</h1>
+
+    <div className="layout">
+      <Stream />
+      <Main />
+    </div>
+
+    <style jsx>{`
+      .layout {
+        display: flex;
+      }
+    `}</style>
   </div>
 )
